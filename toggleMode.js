1e1539492d08007b721d8aa1fc776f650a720db6
dragonpaulz <paul.geesaman@gmail.com>
2020-02-27 14:48:22 -0500

    Got toggle dark mode working"

--- toggleMode.js
+++ toggleMode.js
@@ -1,7 +1,3 @@
-import { element } from "prop-types";
-
-document.getElementById("toggleMode").onclick = toggleMode()
-
 function toggleMode() {
     let webpage = document.body;
     webpage.classList.toggle("dark-mode");
